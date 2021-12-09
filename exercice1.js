ea99fb699f2383fb9b93d2039b78f54240a1455a
Noah2211 <noah.decroix3@gmail.com>
2021-12-09 15:40:33 +0100

    start exercice

--- exercice1.js
+++ exercice1.js
@@ -1,7 +1,12 @@
 // Make it count up on a timer, calling this function
+let counter = 0;
 let seconds = document.getElementById("seconds");
 let countUp = function() {
-    // ?
+    counter+=1;
+    document.getElementById("seconds").innerHTML = counter;
+    setTimeout(function () {
+        countUp();
+    },1000)
 };
 
 // How can you make it stop counting?
